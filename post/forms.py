1dcf7908497e40d21dfc2e5cd9f31336a8ae6d02
axel <denysiuk.isi@gmail.com>
2020-09-03 13:05:16 -0300

    un poco de estilo

--- post/forms.py
+++ post/forms.py
@@ -18,3 +18,7 @@ class ComentarioForm(forms.ModelForm):
     class Meta:
         model = Comentario
         fields = ('texto',)
+
+    def __init__(self, *args, **kwargs):
+        super(ComentarioForm, self).__init__(*args, **kwargs)
+        self.fields["texto"].widget.attrs.update({'class' : 'materialize-textarea','placeholder' : 'Escriba su comentario', 'type' : 'text'})
